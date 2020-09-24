79b07b07ca6a01f8c35810301f7d3474f8262ac4
LDH <dhl@LDHui-MacBook-Pro.local>
2020-09-25 02:37:19 +0900

    Updates

---
+++ precache-manifest.11bed1ca77daf0abce4f99fcdc5db902.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59726dc5e7cbdf2df88481c6be579b18",
+    "url": "/my-trello/index.html"
+  },
+  {
+    "revision": "a349cdf351a3fb64af80",
+    "url": "/my-trello/static/css/2.c9a73f51.chunk.css"
+  },
+  {
+    "revision": "041988190f5d59d55d42",
+    "url": "/my-trello/static/css/main.508263ba.chunk.css"
+  },
+  {
+    "revision": "a349cdf351a3fb64af80",
+    "url": "/my-trello/static/js/2.887f9b9e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-trello/static/js/2.887f9b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041988190f5d59d55d42",
+    "url": "/my-trello/static/js/main.73be80a5.chunk.js"
+  },
+  {
+    "revision": "80ce7a267cffe9c56898",
+    "url": "/my-trello/static/js/runtime-main.96c21f39.js"
+  }
+]);
